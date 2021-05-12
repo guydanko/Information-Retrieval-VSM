517986b7daed02f2c88593284e9993d63efea3f2
noadav15 <noadav96@gmail.com>
2021-05-12 19:18:17 +0300

    inverted_index

--- vsm_ir.py
+++ vsm_ir.py
@@ -17,14 +17,13 @@ nltk.download('punkt')
 
 
 def count_word_in_text(record_number, record_text):
-    punc_list = '.;:!?/\,#@$&)(\'"'
+    punc_list = '-.;:!?/\,#@$&)(\'"'
     replacement = ' ' * len(punc_list)
     remove_digits = str.maketrans('', '', string.digits)
     record_text = record_text.translate(remove_digits)
     record_text = record_text.translate(str.maketrans(punc_list, replacement))
     text_tokens = word_tokenize(record_text)
     tokens_without_sw = [word.lower() for word in text_tokens if not word in stopwords.words()]
-    print(tokens_without_sw)
     for token in tokens_without_sw:
         if token in inverted_index:
             record_dict = inverted_index[token]
@@ -34,28 +33,28 @@ def count_word_in_text(record_number, record_text):
                 record_dict[record_number] = 1
         else:
             inverted_index[token] = {record_number: 1}
-    print(inverted_index)
 
 
 def parse_one_xml_file(doc):
     for record in doc.xpath(".//RECORD"):
-        record_number = record.xpath(".//RECORDNUM/text()")
-        print(record_number)
-        # record_text = ""
-        # record_text += record.findall(".//TITLE/text()")
-        # record_text += record.findall(".//EXTRACT/text()")
-        # record_text += record.findall(".//ABSTRACT/text()")
-        # for topic in record.findall(".//TOPIC/text()"):
-        #     record_text += topic
-        # count_word_in_text(record_number, record_text)
+        record_number = " ".join(record.xpath(".//RECORDNUM/text()"))
+        record_text = ""
+        record_text += " ".join(record.xpath(".//TITLE/text()"))
+        record_text += " ".join(record.xpath(".//EXTRACT/text()"))
+        record_text += " ".join(record.xpath(".//ABSTRACT/text()"))
+        record_text += " ".join(record.xpath(".//TOPIC/text()"))
+        # for topic in record.xpath(".//TOPIC/text()"):
+        #     print(topic)
+        #     record_text += " ".join(topic)
+        count_word_in_text(record_number, record_text)
+
+    print(inverted_index)
 
 
 def build_inverted_index(path):
     for filename in os.listdir(path):
-        print(path + "\\" + filename)
-        root = etree.parse("cf74.xml")
-        print(etree.tostring(root))
-        # parse_one_xml_file(doc)
+        root = etree.parse(path + "\\" + filename)
+        parse_one_xml_file(root)
 
 
 def main():
